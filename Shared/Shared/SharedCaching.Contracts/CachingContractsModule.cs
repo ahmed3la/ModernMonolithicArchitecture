5f96ed67f48b39b958b632f4fd76f58b9a262733
Ahmed Ola <a.ola.sur@ncgr.gov.sa>
2023-11-05 01:47:10 +0300

    Add SharedCaching Module

---
+++ Shared/Shared/SharedCaching.Contracts/CachingContractsModule.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.Application;
+using Volo.Abp.Modularity;
+using Volo.Abp.Authorization;
+
+namespace SharedCaching.Contracts
+{
+    [DependsOn(
+    typeof(AbpDddApplicationContractsModule),
+    typeof(AbpAuthorizationModule)
+    )]
+
+
+    public class CachingContractsModule : AbpModule
+    {
+
+    }
+}
---
+++ Shared/Shared/SharedCaching/CachingModule.cs
@@ -0,0 +1,28 @@
+﻿ using Volo.Abp.AspNetCore.Serilog;
+using Volo.Abp.Autofac;
+using Volo.Abp.Caching.StackExchangeRedis;
+using Volo.Abp.Data;  
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity; 
+using Volo.Abp.Swashbuckle;
+
+
+namespace SharedCaching
+{
+    [DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AbpDataModule),
+    typeof(AbpCachingStackExchangeRedisModule),
+    typeof(AbpAspNetCoreSerilogModule),
+    typeof(AbpSwashbuckleModule)
+    )]
+    public class CachingModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+             
+        }
+         
+
+    }
+}
